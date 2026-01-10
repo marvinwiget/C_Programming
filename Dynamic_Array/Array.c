8a51e06b5bc1dae446bfd926e593fb1e6f22047b
Marvin <marwiget@gmail.com>
2026-01-10 17:49:31 +0100

    Updated Array.c

--- Dynamic_Array/Array.c
+++ Dynamic_Array/Array.c
@@ -5,15 +5,31 @@ typedef struct { int *data, size, capacity; } Array;
 
 void arr_init(Array *arr, int n); // initializes the array with the starting size
 void arr_append(Array *arr, int x); // adds an integer at the end of the array
+void arr_appendArray(Array *arr, int *other, int size); // adds the element of another array at the end of the array
 void arr_removeAt(Array *arr, int index); // removes the integer at the index
 int  arr_pop(Array *arr); // removes the integer at the end of the array
+void arr_print(Array *arr); // prints the whole array
 void arr_destroy(Array *arr); // destroys the array
 
 
 int main() {
 	Array arr;
 	arr_init(&arr, 5);
+	arr_print(&arr);
+
 	arr_append(&arr, 10);
+	arr_append(&arr, 5);
+	arr_print(&arr);
+
+	int newArray[] = {4,3,2,1};
+	int newArraySize = sizeof(newArray) / sizeof(int);
+	
+
+
+	arr_appendArray(&arr, &newArray[0], newArraySize);
+	arr_print(&arr);
+
+	
 
 
 	arr_destroy(&arr);
@@ -28,7 +44,19 @@ void arr_init(Array *arr, int n) {
 }
 
 void arr_append(Array *arr, int x) {
-	return;
+	if (arr->size == arr->capacity) {
+		arr->capacity *= 2;
+		arr->data = malloc(arr->capacity * sizeof(int));
+	}
+
+	arr->data[arr->size] = x;
+	arr->size++;
+}
+
+void arr_appendArray(Array *arr, int *other, int size) {
+	for (int i=0; i<size; i++) {
+		arr_append(arr, other[i]);
+	}
 }
 
 void arr_removeAt(Array *arr, int index) {
@@ -36,7 +64,21 @@ void arr_removeAt(Array *arr, int index) {
 }
 
 int arr_pop(Array *arr) {
-	return 0;
+	int temp = arr->data[arr->size];
+	arr->size--;
+	return temp;
+}
+
+void arr_print(Array *arr) {
+	printf("[");
+	for (int i=0; i<arr->size; i++) {
+		if (i == arr->size-1) {
+			printf("%d", arr->data[i]);
+		} else {
+			printf("%d, ", arr->data[i]);
+		}
+	}
+	printf("]\n");
 }
 
 void arr_destroy(Array *arr) {
