00a1d496f731708c53586e65f7ede143bf46579c
Marvin <marwiget@gmail.com>
2026-01-15 12:03:22 +0100

    Some more additions to NN.c

--- ML/NN.c
+++ ML/NN.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdint.h>
+#include <math.h>
 
-#define u32 uint32_t
-#define i32 int32_t
 
-#define N_HIDDEN_LAYERS 5
+#define LEARNING_RATE 0.01
+#define N_TRAINING 100
+#define N_NEURONS 9
 #define INIT_BIAS 0
 
 /* ... */
@@ -15,6 +15,11 @@
 
 double from[] = {-3, -2, -1, 0, 1, 2, 3};
 double to[] = {9, 4, 1, 0, 1, 4, 9};
+
+enum Activation {
+	RELU,
+	SOFTMAX
+};
 
 typedef struct {
 	double *weights;
@@ -26,10 +31,12 @@ typedef struct {
 
 
 void activation_ReLU(double *input, int n);
-void activation_SoftMax(double *input, int n);
+void activation_softmax(double *input, int n);
 
 Hidden_Layer* init_hiddenLayer(int n_input, int n_output);
-void forward(double *prev_output, int n_prev_output, Hidden_Layer *next);
+void forward(double *prev_output, Hidden_Layer *next, enum Activation flag);
+void updateWeights(Hidden_Layer *l);
+void printLayer(Hidden_Layer *l);
 void destroy_hiddenLayer(Hidden_Layer *l);
 
 double Loss();
@@ -37,16 +44,41 @@ double Loss();
 int main() {
 	srand(time(NULL));
 	
-	Hidden_Layer *layer1 = init_hiddenLayer(1, 3);
-	Hidden_Layer *layer2 = init_hiddenLayer(3, 3);
+	Hidden_Layer *layer1 = init_hiddenLayer(1, N_NEURONS);
+	Hidden_Layer *layer2 = init_hiddenLayer(N_NEURONS, N_NEURONS);
+	Hidden_Layer *output_layer = init_hiddenLayer(N_NEURONS, 1);
 
-	forward(&from[0], 7, layer1);
-	forward(layer1->output, layer1->n_output, layer2);
+	for (int T=0; T < N_TRAINING; T++) {
+		for (int i=0; i<7; i++) {
+			enum Activation ACTIVATION_FLAG = RELU;
+
+			forward(&from[i], layer1, ACTIVATION_FLAG );
+			forward(layer1->output, layer2, ACTIVATION_FLAG);
+			ACTIVATION_FLAG = SOFTMAX;
+			forward(layer2->output, output_layer, ACTIVATION_FLAG);
+			
+
+			double error = output_layer->output[0] - to[i];
+			printf("%f - ", output_layer->output[0]);
+			//printf("%f - ", error);
+
+			updateWeights(layer1);
+			updateWeights(layer2);
+			updateWeights(output_layer);
+
+
+		}
+		printf("\n");
+	}
+
+	
+	
 
 
 
 	destroy_hiddenLayer(layer1);
 	destroy_hiddenLayer(layer2);
+	destroy_hiddenLayer(output_layer);
 	return 0;
 }
 
@@ -57,6 +89,21 @@ void activation_ReLU(double *input, int n) {
 		input[i] = (input[i] > 0) ? input[i] : 0;
 	}
 	return;
+}
+
+void activation_softmax(double *input, int n) {
+	double max = input[0];
+	for (int i=0; i<n; i++) {
+		max = (max < input[i]) ? input[i] : max;
+	}
+	double sum = 0;
+	for (int i=0; i<n; i++) {
+		input[i] = exp(input[i] - max);
+		sum += input[i];
+	}
+	for (int i=0; i<n; i++) {
+		input[i] = input[i] / sum;
+	}
 }
 
 Hidden_Layer* init_hiddenLayer(int n_input, int n_output) {
@@ -87,7 +134,7 @@ Hidden_Layer* init_hiddenLayer(int n_input, int n_output) {
 	return l;
 }
 
-void forward(double *prev_output, int n_prev_output, Hidden_Layer *next) {
+void forward(double *prev_output, Hidden_Layer *next, enum Activation flag) {
 	// dot product
 	for (int i=0; i<next->n_output; i++) {
 		double sum = 0.0;
@@ -97,7 +144,24 @@ void forward(double *prev_output, int n_prev_output, Hidden_Layer *next) {
 		next->output[i] = sum + next->biase[i];
 	}
 	// activation function
-	activation_ReLU(next->output, next->n_output);
+	switch(flag) {
+		case RELU:
+			activation_ReLU(next->output, next->n_output);
+			break;
+		case SOFTMAX:
+			//activation_softmax(next->output, next->n_output); //not necessary for this
+			break;
+	}
+}
+
+void updateWeights(Hidden_Layer *l) {
+	for (int i=0; i<l->n_input*l->n_output; i++) {
+		l->weights[i] = (((double)rand() / (double)RAND_MAX) * 2.0 - 1.0);
+	}
+}
+
+void printLayer(Hidden_Layer *l) {
+	
 }
 
 void destroy_hiddenLayer(Hidden_Layer *l) {
@@ -107,3 +171,5 @@ void destroy_hiddenLayer(Hidden_Layer *l) {
 	free(l);
 	return;
 }
+
+
