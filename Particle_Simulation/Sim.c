fc88094a9f38c2a9794822f61f2698917bd1a2f2
Marvin <marwiget@gmail.com>
2026-01-17 15:43:31 +0100

    Added interactions with mouse

--- Particle_Simulation/Sim.c
+++ Particle_Simulation/Sim.c
@@ -1,19 +1,23 @@
+/* gcc -o sim Sim.c `sdl2-config --cflags --libs` && ./sim */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "SDL.h"
 
 #define WIDTH 800
 #define HEIGHT 600
 
-#define PARTICLE_SIZE 10
-#define MAX_SPEED 1
-#define N_PARTICLES 600
+#define PARTICLE_SIZE 3
+#define MAX_SPEED 10
+#define N_PARTICLES 800
 
 #define GRAVITY 0.1
+#define USER_GRAV 0.7
 #define DAMPENING 0.99
+#define COLL_DAMPENING 0.75
 
 #define WHITE 0xFFFFFFFF
 
@@ -25,7 +29,11 @@ typedef struct {
 	uint32_t color;
 } Particle;
 
+// global vars
 Particle **PARTICLES;
+int particles_initialized = 0;
+Particle *user_particle;
+bool USER_GRAV_ACTIVE = false;
 
 Vec2 scaleVec2(float scalar, Vec2 *v) {
 	Vec2 res;	
@@ -51,37 +59,58 @@ Vec2* initVector(float x, float y) {
 	v->y = y;
 	return v;
 }
-void particleCollision(Particle *p) {
+void particleCollision(Particle *p, Particle *other) {
 	Vec2 *pos = p->pos;
 	Vec2 *vel = p->vel;
-	
+	Vec2 *o_pos = other->pos;
+	Vec2 *o_vel = other->vel;
+	float distance = sqrt(pow(pos->x - o_pos->x, 2) +
+			      pow(pos->y - o_pos->y, 2));
+	if (p->radius + other->radius > distance) {
+		if (distance == 0) {
+            		pos->x += 0.1f;
+			distance = 0.1f;
+        	}
+		float overlap = (p->radius + other->radius - distance);
+		// Use a small 'damping' factor (0.5 means each moves half the overlap)
+		float force = 0.5f; 
 
+		// 1. Calculate the direction (Normal)
+		float nx = (pos->x - o_pos->x) / distance;
+		float ny = (pos->y - o_pos->y) / distance;
 
-	for (int i=0; i<N_PARTICLES;i++) {
-		if (PARTICLES[i] == p) continue;
-		Particle *other = PARTICLES[i];
-		Vec2 *o_pos = other->pos;
-		Vec2 *o_vel = other->vel;
+		// 2. Push along the normal
+		pos->x += nx * overlap * force;
+		pos->y += ny * overlap * force;
 
-		float distance = sqrt(pow(pos->x - o_pos->x, 2) +
-				      pow(pos->y - o_pos->y, 2));
-		if (sqrt(pow(distance,2)) < 0.0001f) distance = 0.0001f;
-		if (p->radius + other->radius > distance) {
+		o_pos->x -= nx * overlap * force;
+		o_pos->y -= ny * overlap * force;
 
-			Vec2 minus1 = minusVec2(vel, o_vel);
-			Vec2 minus2 = minusVec2(o_vel, vel);
-			Vec2 minus3 = minusVec2(pos, o_pos);
-			Vec2 minus4 = minusVec2(o_pos, pos);
-			float dot1 = dotProduct(&minus1, &minus3);
-			float dot2 = dotProduct(&minus2, &minus4);
-			Vec2 scale1 = scaleVec2(dot1/pow(distance, 2), &minus3);
-			Vec2 scale2 = scaleVec2(dot2/pow(distance, 2), &minus4);
+		Vec2 minus1 = minusVec2(vel, o_vel);
+		Vec2 minus2 = minusVec2(o_vel, vel);
+		Vec2 minus3 = minusVec2(pos, o_pos);
+		Vec2 minus4 = minusVec2(o_pos, pos);
+		float dot1 = dotProduct(&minus1, &minus3);
+		float dot2 = dotProduct(&minus2, &minus4);
+		Vec2 scale1 = scaleVec2(dot1/pow(distance, 2), &minus3);
+		Vec2 scale2 = scaleVec2(dot2/pow(distance, 2), &minus4);
 
-			*p->vel = minusVec2(vel, &scale1);
+		*p->vel = minusVec2(vel, &scale1);
 
-			*other->vel = minusVec2(o_vel, &scale2);
+		*other->vel = minusVec2(o_vel, &scale2);
 
-		}
+		// enforce max speed
+		p->vel->x = (p->vel->x > 0) ? fmin(MAX_SPEED, p->vel->x) : fmax(-MAX_SPEED, p->vel->x);
+		p->vel->y = (p->vel->y > 0) ? fmin(MAX_SPEED, p->vel->y) : fmax(-MAX_SPEED, p->vel->y);
+
+		other->vel->x = (other->vel->x > 0) ? 
+				fmin(MAX_SPEED, other->vel->x) : fmax(-MAX_SPEED,other->vel->x);
+		other->vel->y = (other->vel->y > 0) ?
+		       		fmin(MAX_SPEED, other->vel->y) : fmax(-MAX_SPEED,other->vel->y);
+		p->vel->x *= COLL_DAMPENING;
+		p->vel->y *= COLL_DAMPENING;
+		other->vel->x *= COLL_DAMPENING;
+		other->vel->y *= COLL_DAMPENING;
 	}
 }
 
@@ -91,13 +120,18 @@ void updateParticle(Particle *p) {
 
 
 	// GRAVITY AND DAMPENING
-	vel->y += (pos->y < HEIGHT - p->radius) ? GRAVITY : 0;
+	vel->y += GRAVITY;
 	vel->x *= DAMPENING;
 	vel->y *= DAMPENING;
-	
-	// Check particle collisions
-	particleCollision(p);
 
+	// USER GRAVITY
+	if (USER_GRAV_ACTIVE) {
+		float gx = user_particle->pos->x;
+		float gy = user_particle->pos->y;
+
+		vel->x += (gx > pos->x) ? USER_GRAV : -USER_GRAV;
+		vel->y += (gy > pos->y) ? USER_GRAV : -USER_GRAV;
+	}	
 
 	// SET NEW POS
 	pos->x += vel->x;
@@ -123,16 +157,33 @@ void updateParticle(Particle *p) {
 
 }
 
-
+int isColliding(int x, int y) {
+	for (int i=0;i<particles_initialized;i++) {
+		Particle *other = PARTICLES[i];
+		float distance = sqrt(pow(x-other->pos->x, 2) + pow(y-other->pos->y, 2));
+		if (distance < PARTICLE_SIZE*2) return 1;
+	}
+	return 0;
+}
 
 Particle* initParticle() {
 	Particle *p = malloc(sizeof(Particle));
-	int rand_x = rand() % WIDTH;
-	int x_pos = (rand_x < PARTICLE_SIZE) ? PARTICLE_SIZE :
-	     	    (rand_x > WIDTH - PARTICLE_SIZE) ? WIDTH - PARTICLE_SIZE : rand_x;
-	int rand_y = rand() % HEIGHT;
-	int y_pos = (rand_y < PARTICLE_SIZE) ? PARTICLE_SIZE : 
-		    (rand_y > HEIGHT - PARTICLE_SIZE) ? HEIGHT - PARTICLE_SIZE : rand_y;
+
+	// random start pos
+	int x_pos;
+	int y_pos;
+	int attemps = 0;
+	do {
+		int rand_x = rand() % WIDTH;
+		x_pos = (rand_x < PARTICLE_SIZE) ? PARTICLE_SIZE :
+	     	        (rand_x > WIDTH - PARTICLE_SIZE) ? WIDTH - PARTICLE_SIZE : rand_x;
+
+		int rand_y = rand() % HEIGHT;
+		y_pos = (rand_y < PARTICLE_SIZE) ? PARTICLE_SIZE : 
+		        (rand_y > HEIGHT - PARTICLE_SIZE) ? HEIGHT - PARTICLE_SIZE : rand_y;
+		attemps++;
+		if (attemps > 1000) printf("no space for particles"); break;
+	} while (isColliding(x_pos, y_pos));
 	p->pos = initVector(x_pos, y_pos);
 	p->vel = initVector(rand() % MAX_SPEED, rand() & MAX_SPEED);
 	p->radius = PARTICLE_SIZE;
@@ -141,9 +192,10 @@ Particle* initParticle() {
 }
 
 Particle** initParticles() {
-	Particle **PARTICLES = malloc(sizeof(Particle*) * N_PARTICLES);
+	PARTICLES = malloc(sizeof(Particle*) * N_PARTICLES);
 	for (int i=0; i<N_PARTICLES; i++) {
 		PARTICLES[i] = initParticle();
+		particles_initialized++;
 	}
 	return PARTICLES;
 }
@@ -164,6 +216,9 @@ void drawParticle(SDL_Surface *surface, Particle *p) {
 
 void ALL_PARTICLES(SDL_Surface *surface, Particle **p) {
 	for (int i=0;i<N_PARTICLES;i++) {
+		for (int j=0;j<N_PARTICLES;j++) {
+			if (j > i) particleCollision(p[i], p[j]);
+		}
 		updateParticle(p[i]);
 		drawParticle(surface, p[i]);
 	}
@@ -173,17 +228,41 @@ int main(int argc, char *argv[]) {
 	SDL_Window *window = SDL_CreateWindow("Particle Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 	SDL_Surface *surface = SDL_GetWindowSurface(window);
 	srand(time(NULL));
+
 	PARTICLES = initParticles();
+	// init gravity particle
+	user_particle = initParticle();
+	user_particle->pos = initVector(0,0);
+	user_particle->vel = initVector(0,0);
+	user_particle->radius = 30;
+	user_particle->color = 0xABCDEFAB;
 
 	// main loop 
 	int running = 1;
 	SDL_Event event;
+
 	while (running) {
-		while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) running = 0;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) running = 0;
+			if (event.type == SDL_MOUSEBUTTONDOWN) {
+				if (event.button.button == SDL_BUTTON_LEFT) {
+					user_particle->pos->x = event.button.x;
+					user_particle->pos->y = event.button.y;
+					USER_GRAV_ACTIVE = true;
+				}
+			}
+			if (event.type == SDL_MOUSEBUTTONUP) {
+				if (event.button.button == SDL_BUTTON_RIGHT) {
+					USER_GRAV_ACTIVE = false;
+				}
+			}
+			
+		}
 		SDL_Rect bg = (SDL_Rect){0,0,WIDTH,HEIGHT};
 		SDL_FillRect(surface, &bg, 0x00000000);	
 
 		ALL_PARTICLES(surface, PARTICLES);
+		if (USER_GRAV_ACTIVE) drawParticle(surface, user_particle);
 		
 		SDL_UpdateWindowSurface(window);
 		SDL_Delay(16);	 // 16
